ac3e8d12757ca3403f4be7571710075f16b620ea
Toby9210 <mikailfaysaltobi@gmail.com>
2023-02-21 14:29:19 -0800

    0-putchar.c
    git commit -m 0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,13 +2,13 @@
 
 
 /**
- * _purchar - write the character c to stdout
+ * _putchar - write the character c to stdout
  * @c: The character to print
  * Return: 0n success 1.
  * 0n error, -1 is returned, and errno is set appropriately.
  */
 
-int _purchar(char c)
+int _putchar(char c)
 
 {
 
