ebf8ea8c9b902f47157dbc064c34d13fc2036f9f
Toby9210 <mikailfaysaltobi@gmail.com>
2023-02-23 17:37:59 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_triangles - Prints a triangle of squares according to parameter
+ * print_triangle - Prints a triangle of squares according to parameter
  * @size: The size of the squares triangle
  * Return: empty
  */
